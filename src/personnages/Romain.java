9ab5933e352e44bc3e52422a2c27c4a11add826f
ctrlpierre <pierre.parodi@univ-tlse3.fr>
2024-09-24 10:03:13 +0200

    TP1

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
+		assert force > 0 : "La force d'un Romain est toujours positive.";
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 	
 	public String getNom() {
@@ -22,11 +26,37 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert force > 0 : "La force d'un Romain est toujours positive.";
+		int forceAvant = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 		parler("J'abandonne...");
+		}
+		assert forceAvant > force : "La force du Romain n'as pas diminué.";
+	}
+	
+	public void sEquiper(Equipement e) {
+		switch (nbEquipement) {
+			case 2:
+				System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+				break;
+			case 1:
+				if (equipements[0] == e) {
+					System.out.println("Le soldat " + nom + " possède déjà un " + e + " !");
+				} 
+				else {
+					equipements[1] = e;
+					System.out.println("Le soldat " + nom + " s'équipe avec un " + e + ".");
+					nbEquipement++;
+				}
+				break;
+			case 0:
+				equipements[0] = e;
+				System.out.println("Le soldat " + nom + " s'équipe avec un " + e + ".");
+				nbEquipement++;
+				break;
 		}
 	}
 
@@ -36,9 +66,14 @@ public class Romain {
 	}
 	
 	public static void main(String[] args) {
-		Romain cesar = new Romain("César", 5);
-		System.out.println(cesar);
-		cesar.parler("Je vais me frapper !");
-		cesar.recevoirCoup(69420);
+		Romain minus = new Romain("Minus", 6);
+		System.out.println(minus);
+		minus.parler("Je vais me frapper !");
+		minus.recevoirCoup(69420);
+		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,17 +2,68 @@ package personnages;
 
 public class Village {
 	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 	
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 	
 	public String getNom() {
 		return nom;
 	}
-
-	@Override
-	public String toString() {
-		return "Village [nom=" + nom + "]";
+	
+	public int getNbVillageois() {
+		return nbVillageois;
+	}
+	
+	public void ajouterHabitant(Gaulois g, int place) {
+		assert 0 <= place && place < villageois.length : "La place est strictement < au nbMax de villageois.";
+		villageois[place] = g;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int nbVillageois) {
+		return villageois[nbVillageois];
+	}
+	
+	public void afficherVillageois() {
+		if (chef != null) {
+			System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+		} else {
+			System.out.println("Dans ce village sans chef vivent les légendaires gaulois :");
+		}
+		for (int i = 0; i < villageois.length; i++) {
+			if (villageois[i] != null) {
+				System.out.println("- " + villageois[i].getNom());
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		Index out of range
+		
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(asterix, 0);
+		village.ajouterHabitant(obelix, 1);
+		village.afficherVillageois();
+		
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//		En effet, on a mis Astérix en position 0 et on le cherche en 
+//		position 1, cela renvoie donc null car on a pas initialisé 
+//		le villageois d'indice 1		
 	}
 }
